d9ffd7e2466b9606baaefbd69759f405f0cbeeeb
lijianru <2219269374@qq.com>
2024-07-07 16:56:53 +0800

    init repo

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import IconsResolver from 'unplugin-icons/resolver';
+import Icons from 'unplugin-icons/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,10 +16,40 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
+    // 自动导入
+    AutoImport({
+      // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+      imports: ['vue'],
+      eslintrc: {
+        enabled: false,
+        filepath: './.eslintrc-auto-import.cjs',
+      },
+      resolvers: [
+        // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+        ElementPlusResolver(),
+        // 自动导入图标组件
+        IconsResolver({}),
+      ],
+    }),
+    Components({
+      resolvers: [
+        // 自动导入 Element Plus 组件
+        ElementPlusResolver(),
+        // 自动注册图标组件
+        IconsResolver({
+          // element-plus图标库，其他图标库 https://icon-sets.iconify.design/
+          enabledCollections: ['ep'],
+        }),
+      ],
+    }),
+    Icons({
+      // 自动安装图标库
+      autoInstall: true,
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
